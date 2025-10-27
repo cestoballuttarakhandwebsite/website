2d0a47c48def1bf265d8e1b843886382a9fde73c
Cestoball Uttarakhand <cestoballuttarakhandwebsite@gmail.com>
2025-10-27 20:45:26 +0530

    initial hero section build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,56 @@ import chairmanPhoto from '@/assets/ShukdevSingh.jpg'
 import presidentPhoto from '@/assets/PoonamPalival.jpg'
 import treasurerPhoto from '@/assets/LaxmiRani.jpg'
 import generalSecretary from '@/assets/Shashikant.jpg'
+import car1 from '@/assets/HomeCarousal1.jpg'
+import car2 from '@/assets/HomeCarousal2.jpg'
+import car3 from '@/assets/HomeCarousal3.jpg'
+
+// shadcnimports
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 
 export default function Home() {
   return (
     <>
+
+    {/* heroSection */}
+
+    <div className="flex w-full h-[15rem] sm:h-[calc(100vh-(13rem))] mb-0">
+      <div className="w-full sm:w-2/3">
+        <Carousel className="w-full sm:mt-10 sm:ml-14 sm:w-202">
+          <CarouselContent>
+            <CarouselItem>
+              <Image
+              src={car1}
+              alt="image"/>
+            </CarouselItem>
+            <CarouselItem>
+              <Image
+              src={car2}
+              alt="image"
+              height={500}/>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious className="hidden sm:flex"/>
+          <CarouselNext className="hidden sm:flex"/>
+        </Carousel>
+      </div>
+      <div className="hidden sm:flex sm:w-1/3">
+
+      </div>
+    </div>
+
     {/* gameAboutSection */}
+  
 
-
-    <div className=" w-full flex sm:h-[calc(100vh-(6rem))]">
+    <div className=" w-full flex sm:h-[calc(100vh-(14rem))]">
       <div className="flex w-1/2 sm:w-2/3">
         <div className="mx-auto my-auto">
           <div className="text-lg sm:text-5xl font-bold text-[#150EA2] text-center">
@@ -125,6 +166,7 @@ export default function Home() {
         </div>
 
       </div>
+
     </div>
 
 
